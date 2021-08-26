cce06cb28343d2ee6d5359d1923e14b45153859c
idilsaglam <idilsaglam1@hotmail.com>
2021-08-26 14:12:36 +0300

    ss generator created

---
+++ src/main/java/io/makepad/Generator.java
@@ -0,0 +1,43 @@
+package io.makepad;
+
+import com.google.common.collect.ClassToInstanceMap;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.print.PageSize;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.imageio.ImageIO;
+import javax.swing.text.Document;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.Duration;
+
+public class Generator {
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+
+    public Generator() throws IOException {
+        FirefoxOptions options = new FirefoxOptions();
+        this.driver = new FirefoxDriver(options);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(120));
+        this.driver.manage().window().maximize();
+        this.driver.get("https://www.mozilla.org/fr/about/");
+
+    }
+
+    public void takeSS(String xpath) throws IOException {
+        WebElement element = this.driver.findElement(By.xpath(xpath));
+        // capture screenshot with getScreenshotAs() of the dropdown
+        File f = element.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(f, new File("/Users/idilsaglam/Documents/Makepad/SSgenerator/test101.png"));
+        driver.quit();
+    }
+}
---
+++ src/main/java/io/makepad/Test.java
@@ -0,0 +1,8 @@
+package io.makepad;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Generator g = new Generator();
+        g.takeSS("//*[@id='c-navigation-items']");
+    }
+}
