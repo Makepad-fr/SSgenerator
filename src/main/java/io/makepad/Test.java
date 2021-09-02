83ec4b7d3309ff2eb394bc086c8745360c9d6f0b
idilsaglam <idilsaglam1@hotmail.com>
2021-09-02 20:52:12 +0300

    Test class updated

--- src/main/java/io/makepad/Test.java
+++ src/main/java/io/makepad/Test.java
@@ -3,6 +3,6 @@ package io.makepad;
 public class Test {
     public static void main(String[] args) throws Exception {
         Generator g = new Generator();
-        g.takeSS("//*[@id='c-navigation-items']");
+        g.takeSS("https://www.mozilla.org/fr/about/","//*[@id='c-navigation-items']","/Users/idilsaglam/Documents/Makepad/SSgenerator/test2222.png");
     }
 }
